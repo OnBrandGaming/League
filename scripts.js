bd26de5e9677d018819bcc428251e7d48754388a
OBGL <107322592+OnBrandGaming@users.noreply.github.com>
2023-02-08 11:57:54 -0500

    Update scripts.js
    updated countdown to new tournament (UFC in Apr 2023)

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ Table of Contents
  *****************/
 
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 5, 2023 18:00:00").getTime();
+var countDownDate = new Date("Apr 8, 2023 17:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
